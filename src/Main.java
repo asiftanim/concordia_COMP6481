39a9ec2b70d3e76ebcd4b2424154cc7310bc46a5
ASIF TANIM <tanimasif23@gmail.com>
2022-02-20 19:09:27 -0500

    Assignment done

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,22 @@
+//.......................................
+// Assignment 1
+// © Kazi Asif Tanim
+// Written by: Kazi Asif Tanim, 40216618
+// Type: Driver class
+// Description: Vaccine inventory program. Add/Update/Search/View Vaccine records.
+//.......................................
+
 import java.util.Scanner;
 
 public class Main {
 	
-	private static final String password = "password";
-	private static int wrongPasswordCounter = 0;
-	static Vaccine[] inventory;
+	private static final String password = "password"; //static password
+	private static int wrongPasswordCounter = 0; //wrong password counter
+	static Vaccine[] inventory; // Vaccine of Vaccine which will be used as inventory
 	
+	//method to handle password. isTerminate will decide if it should consider 12 failed attempt or not
 	public static boolean checkPassword(boolean isTerminate) {
-		System.out.println("Please enter your password");
+		System.out.println("\nPlease enter your password");
 		Scanner scanner = new Scanner(System.in);
 		String pass = scanner.nextLine();
 		boolean output = false;
@@ -15,14 +24,15 @@ public class Main {
 		if(pass.equals(password)) {
 			output = true;
 		}else {
-			wrongPasswordCounter++;
-			if(wrongPasswordCounter == 12 && isTerminate) {
+			System.out.println("Wrong password");
+			wrongPasswordCounter++; //wrong password counter
+			if(wrongPasswordCounter == 12 && isTerminate) { // check if user input 12 wrong password.
 				System.out.println("Program  detected  suspicious  activities  and  will  terminate  immediately!");
-			   System.exit(0);
-			}else if(wrongPasswordCounter == 3 || wrongPasswordCounter == 6 || wrongPasswordCounter == 9) {
+			   System.exit(0); //terminate the program as 12 failed attempt
+			}else if(wrongPasswordCounter % 3 == 0) { //This is for redisplay main menu for if 3 password attemps failed
 				output = false;
 			}else {
-				if(checkPassword(isTerminate)) {
+				if(checkPassword(isTerminate)) { //recursion to keep asking for password if password is wrong
 					output = true;
 				};
 			}
@@ -30,34 +40,38 @@ public class Main {
 		return output;
 	}
 	
+	//Method to find Vaccine by brand name. User a loop and match given brand. if exist print vaccine info
 	public static void findVaccineBy(Vaccine.Brands brand) {
 		for(int i=0;i <Vaccine.getNumberOfCreatedVaccine(); i++) {
 			if(inventory[i].getBrand() == brand) {
-				System.out.println("Vaccine: #" + i);
+				System.out.println("\nVaccine: #" + i);
 				System.out.println(inventory[i]);
 			}
 		}
 	}
 	
+	//Method to find Vaccine cheaper than give price. User a loop and match given price. if exist print vaccine info
 	public static void findCheaperThan(double price) {
 		for(int i=0;i <Vaccine.getNumberOfCreatedVaccine(); i++) {
 			if(inventory[i].getPrice_tag() < price) {
-				System.out.println("Vaccine: #" + i);
+				System.out.println("\nVaccine: #" + i);
 				System.out.println(inventory[i]);
 			}
 		}
 	}
 
+	//Entry point of the program.
 	public static void main(String[] args) {		
 		
-		System.out.println("Welcome User");
+		System.out.println("Welcome User \n");
 		System.out.println("How many vaccines your store will contain?");
 		Scanner scanner = new Scanner(System.in);
 		
-		inventory = new Vaccine[Integer.parseInt(scanner.nextLine())];
+		inventory = new Vaccine[Integer.parseInt(scanner.nextLine())]; //Init Vaccine array size by user given size.
 		
+		//infite loop to keep keep the program running unit user Quit.
 		while(true) {
-			System.out.println("What do you want to do?");
+			System.out.println("\nWhat do you want to do?");
 			System.out.println("1. Enter new vaccines (password required)");
 			System.out.println("2. Change information of a vaccine (password required)");
 			System.out.println("3. Display all vaccines by a specific brand");
@@ -65,25 +79,27 @@ public class Main {
 			System.out.println("5. Quit");
 			System.out.println("Please enter your choice >");
 			
-			int choice = Integer.parseInt(scanner.nextLine());
+			int choice = Integer.parseInt(scanner.nextLine()); //get menu choice from user
 			
+			//switch cases from user input.
 			switch(choice) {
 				case 1:
-					
-					if(checkPassword(true)) {
-						wrongPasswordCounter = 0;
+					if(checkPassword(true)) { //check password. continue if password is right. all password situation is handled in checkPassword method
+						wrongPasswordCounter = 0; // wrong pass counter to 0 as password is correct.
 						System.out.println("How many vaccines do you want to add?");
 						int numOfVaccine = Integer.parseInt(scanner.nextLine());
 						
+						//check if there is enough storage. new vaccine count should be less than or equal (inventory size - number of existng vaccine)
 						if(numOfVaccine <= (inventory.length - Vaccine.getNumberOfCreatedVaccine())) {
 							
-							for(int i=0; i<numOfVaccine; i++) {
-								Vaccine v = new Vaccine();
-								System.out.println("Enter vaccine ID");
-								v.setId(Integer.parseInt(scanner.nextLine()));
+							for(int i=0; i<numOfVaccine; i++) { //loop to add all the vaccines
+								
+								System.out.println("\nEnter vaccine ID");
+								Vaccine v = new Vaccine(Integer.parseInt(scanner.nextLine())); //assign id when creating object
 								
 								System.out.println("Enter choose vaccine Brand from below: ");
-								System.out.println("1. PFIZER \n2. MODERNA \n3.ASTRAZENICA ");
+								System.out.println("1. PFIZER \n2. MODERNA \n3. ASTRAZENICA ");
+								//set brand name accourding to user choice
 								int c = Integer.parseInt(scanner.nextLine());
 								switch(c) {
 									case 1:
@@ -100,20 +116,21 @@ public class Main {
 								System.out.println("Enter Dose");
 								v.setDose(Double.parseDouble(scanner.nextLine()));
 								
-								System.out.println("Enter Expirty Date");
+								System.out.println("Enter Expiry Date");
 								v.setExpiry_date(scanner.nextLine());
 								
 								System.out.println("Enter price");
 								v.setPrice_tag(Double.parseDouble(scanner.nextLine()));
 								
-								inventory[Vaccine.getNumberOfCreatedVaccine() - 1] = v;
+								inventory[Vaccine.getNumberOfCreatedVaccine() - 1] = v; // Assign  Vaccine object v to the inventory. index will be (exsting vaccine - 1)
 								
-								System.out.println(inventory[Vaccine.getNumberOfCreatedVaccine() - 1]);
+								System.out.println("\nVaccine: #" + (Vaccine.getNumberOfCreatedVaccine() - 1)); //print vaccine index number
+								System.out.println(inventory[Vaccine.getNumberOfCreatedVaccine() - 1]); // print whole vaccine object
 							}
 	
 							
 							
-						}else {
+						}else { //If no storage show a message and show how many space left.
 							System.out.println("Not enough storage. You can only add " + (inventory.length - Vaccine.getNumberOfCreatedVaccine()) + " Vaccine");
 							break;
 						}
@@ -121,12 +138,128 @@ public class Main {
 						break;
 					}
 					
-					
 					break;
 				case 2:
-					System.out.println("change");
+					if(checkPassword(false)) { //check password. but this time 12 failed attampt condition will not be checked.
+						wrongPasswordCounter = 0; //success login so counter to 0;
+						System.out.println("\nPlease enter vaccine index number which you want to update");
+						int vaccIndex = Integer.parseInt(scanner.nextLine());
+						
+						if(vaccIndex >= inventory.length) { //check to avoid array out of index exception
+							System.out.println("Please input index between 0-" + (inventory.length-1));
+						}
+						
+						//if selected index is empty then ask to enter new vaccine or go to main menu.
+						else if(inventory[vaccIndex] == null) {
+							System.out.println("\nNo vaccine in this index.");
+							System.out.println("1. Add a new vaccine");
+							System.out.println("2. Go to main menu");
+						    int emptySlotChoice = Integer.parseInt(scanner.nextLine());
+						    switch(emptySlotChoice) {
+							    case 1: //Add new vaccine selected.
+							    	
+							    	//same procedure as New vaccine add as above
+									System.out.println("\nEnter vaccine ID");
+									Vaccine v = new Vaccine(Integer.parseInt(scanner.nextLine())); //assign id when creating object
+									
+									System.out.println("Enter choose vaccine Brand from below: ");
+									System.out.println("1. PFIZER \n2. MODERNA \n3. ASTRAZENICA ");
+									int c = Integer.parseInt(scanner.nextLine());
+									switch(c) {
+										case 1:
+											v.setBrand(Vaccine.Brands.PFIZER);
+											break;
+										case 2:
+											v.setBrand(Vaccine.Brands.MODERNA);
+											break;
+										case 3:
+											v.setBrand(Vaccine.Brands.ASTRAZENICA);
+											break;
+									}
+									
+									System.out.println("Enter Dose");
+									v.setDose(Double.parseDouble(scanner.nextLine()));
+									
+									System.out.println("Enter Expiry Date");
+									v.setExpiry_date(scanner.nextLine());
+									
+									System.out.println("Enter price");
+									v.setPrice_tag(Double.parseDouble(scanner.nextLine()));
+									
+									inventory[Vaccine.getNumberOfCreatedVaccine() - 1] = v;
+									
+									System.out.println("\nVaccine: #" + (Vaccine.getNumberOfCreatedVaccine() - 1));
+									System.out.println(inventory[Vaccine.getNumberOfCreatedVaccine() - 1]);
+									
+							    	break;
+							    default: //go to main menu
+							    	break;
+						    }
+						}else { //Vaccine exist in the give slot. Promt user to update.
+							System.out.println("\nVaccine: #" + vaccIndex);
+							System.out.println(inventory[vaccIndex]); //print current vaccine info
+							
+							boolean isLoop = true; // loop flag to exit to main menu as we will enter a infinite loop.
+							
+							while(isLoop) { //keep promting to update from 1-4 until user choose option 5.
+								System.out.println("\nWhat information  would you like to change");
+								System.out.println("1. Brand");
+								System.out.println("2. Dose");
+								System.out.println("3. Expiry");
+								System.out.println("4. price");
+								System.out.println("5. Quit");
+								System.out.println("Enter your choice >");
+								int c = Integer.parseInt(scanner.nextLine());
+								
+								switch(c) { //switch case according to user choice
+									case 1: //update brand name
+										System.out.println("\nEnter choose vaccine Brand from below: ");
+										System.out.println("1. PFIZER \n2. MODERNA \n3.ASTRAZENICA ");
+										int updateChoice = Integer.parseInt(scanner.nextLine());
+										switch(updateChoice) {
+											case 1:
+												inventory[vaccIndex].setBrand(Vaccine.Brands.PFIZER);
+												break;
+											case 2:
+												inventory[vaccIndex].setBrand(Vaccine.Brands.MODERNA);
+												break;
+											case 3:
+												inventory[vaccIndex].setBrand(Vaccine.Brands.ASTRAZENICA);
+												break;
+										}
+										System.out.println("\nVaccine: #" + vaccIndex);
+										System.out.println(inventory[vaccIndex]);
+										break;
+									case 2: //update dose
+										System.out.println("\nEnter Dose");
+										inventory[vaccIndex].setDose(Double.parseDouble(scanner.nextLine()));
+										System.out.println("Vaccine: #" + vaccIndex);
+										System.out.println(inventory[vaccIndex]);
+										break;
+									case 3: //update expiry date
+										System.out.println("\nEnter Expiry Date");
+										inventory[vaccIndex].setExpiry_date(scanner.nextLine());
+										System.out.println("Vaccine: #" + vaccIndex);
+										System.out.println(inventory[vaccIndex]);
+										break;
+									case 4: //update price
+										System.out.println("\nEnter price");
+										inventory[vaccIndex].setPrice_tag(Double.parseDouble(scanner.nextLine()));
+										System.out.println("Vaccine: #" + vaccIndex);
+										System.out.println(inventory[vaccIndex]);
+										break;
+									case 5: //exit infinite loop and go back to main menu.
+										isLoop = false;
+										break;
+									default: //if input is out of range 1-5
+										System.out.println("\nPLease choose between 1-5");
+										break;
+								}
+							}
+						}
+					}
 					break;
-				case 3:
+				case 3: //find vaccine by brand name
 					System.out.println("Enter Brand name");
 					System.out.println("1. PFIZER \n2. MODERNA \n3.ASTRAZENICA ");
 					int c = Integer.parseInt(scanner.nextLine());
@@ -143,16 +276,16 @@ public class Main {
 					}
 					
 					break;
-				case 4:
+				case 4: //find cheap vaccine by given price 
 					System.out.println("Please enter price");
 					findCheaperThan(Double.parseDouble(scanner.nextLine()));
 					break;
-				case 5:
-					System.out.println("Thanks for using the system.");
+				case 5: //exit program
+					System.out.println("\nProgram terminated. Thanks for using the system.");
 					System.exit(0);
 					break;
-				default:
-					System.out.println("Please enter between 1-5");
+				default: //if input is out of 1-5
+					System.out.println("\nPlease enter between 1-5");
 			}
 		}
 		
--- src/Vaccine.java
+++ src/Vaccine.java
@@ -1,7 +1,14 @@
+//.......................................
+// Assignment 1
+// © Kazi Asif Tanim
+// Written by: Kazi Asif Tanim, 40216618
+// Type: Vaccine class
+//.......................................
 
 public class Vaccine {
 	
-	public enum Brands {NONE, PFIZER, MODERNA, ASTRAZENICA};
+	public enum Brands {NONE, PFIZER, MODERNA, ASTRAZENICA}; //Enum of vaccine names
+	
 	private Brands brand;
 	private double dose;
 	private String expiry_date;
@@ -9,39 +16,50 @@ public class Vaccine {
 	private double price_tag;
 	private static int numberOfCreatedVaccine = 0;
 	
+	//Default conostructor
 	public Vaccine() {
 		
+		//Initialize to default vaule
 		brand = Brands.NONE;
 		dose = 0;
 		expiry_date = "";
 		id = 0;
 		price_tag = 0;
-		numberOfCreatedVaccine++;
+		numberOfCreatedVaccine++; //increment to keep track of number of vaccine object created.
 	}
 	
-	public Vaccine(Brands brand, double dose, String expiry_date, double price_tag) {
+	//Parametarized constructor
+	public Vaccine(long id, Brands brand, double dose, String expiry_date, double price_tag) {
+		
+		//Init parameter values
+		this.id = id;
 		this.brand = brand;
 		this.dose = dose;
 		this.expiry_date = expiry_date;
 		this.price_tag = price_tag;
-		numberOfCreatedVaccine++;
+		numberOfCreatedVaccine++; //increment to keep track of number of vaccine object created.
 	}
 	
+	//Init id at the time of object creation. Not allowed to change after assignment
+	public Vaccine(long id) {
+		this.id = id;
+		numberOfCreatedVaccine++; //increment to keep track of number of vaccine object created.
+	}
+	
+	//Copy constructor
 	public Vaccine(Vaccine v) {
 		this.brand = v.brand;
 		this.dose = v.dose;
 		this.expiry_date = v.expiry_date;
 		this.id = v.id;
 		this.price_tag = v.price_tag;
-		numberOfCreatedVaccine++;
+		numberOfCreatedVaccine++; //increment to keep track of number of vaccine object created.
 	}
 	
+	//Getters & Setters start
+	//............................
 	public long getId() {
 		return id;
-	}
-
-	public void setId(long id) {
-		this.id = id;
 	}
 	
 	public Brands getBrand() {
@@ -75,33 +93,42 @@ public class Vaccine {
 	public void setPrice_tag(double price_tag) {
 		this.price_tag = price_tag;
 	}
+	//Getter and Setter end
+	//.......................
 	
+	//method to print vaccine object count
 	public void findNumberOfCreatedVaccines() {
 		System.out.println(numberOfCreatedVaccine);
 	}
 	
+	//method to get vaccine object count
 	public static int getNumberOfCreatedVaccine() {
 		return numberOfCreatedVaccine;
 	}
 	
+	//Override toString to print customize Vaccine object
 	@Override
     public String toString() {
         return "ID: " + this.id + "\nBrand: " + this.brand.toString() + "\nDose: " + this.dose + "\nExpiry: " + this.expiry_date + "\nPrice: $" + this.price_tag;
     }
 	
+	//Override equals to compare between 2 Vaccine object
 	@Override
 	public boolean equals(Object o) {
+		
+		//if compared with same vaccine object
 		if(o == this) {
 			return true;
 		}
 		
+		//Check if vaccine object or not. If different object then return false
 		if (!(o instanceof Vaccine)) {
             return false;
         }
 		
-		Vaccine v = (Vaccine) o;
+		Vaccine v = (Vaccine) o; //type casting Object to Vaccine to compare
 		
-		return brand.equals(v.brand) && dose == v.dose; 
+		return brand.equals(v.brand) && dose == v.dose; //compare then return result
 		
 	}
 }
